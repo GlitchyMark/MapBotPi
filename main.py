dcb097e418d586617a847c2bf187dbb600b21dc0
xander-will <xander@cavecreations.net>
2019-04-06 20:11:22 -0400

    debug stuff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
+
 import robot
+
 from gpiozero import Button
 
 start_button = Button(1)    # update with pin number
@@ -6,7 +9,11 @@ r = robot.Robot()
 
 def main():
     while True:
-        r.run()
+        if len(sys.argv) > 1 and sys.argv[1] == "debug":
+            r.logic.debug()
+        else:
+            r.run()
+
 
 if __name__ == "__main__":
     start_button.when_pressed = main
