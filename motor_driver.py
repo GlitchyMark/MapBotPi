211a67e01da61a076dc74704eb3a6c5443f67d20
MJ9001 <mark.johnston9001@gmail.com>
2019-04-07 15:03:45 -0400

    Support for python3

--- motor_driver.py
+++ motor_driver.py
@@ -17,78 +17,78 @@ class MotorDriverInterface:
 
     def setMotionAllowed(self, value):
         if value is True:
-            self.port.write("[setMotionAllowed=1]")
+            self.port.write(b"[setMotionAllowed=1]")
         else:
-            self.port.write("[setMotionAllowed=0]")
+            self.port.write(b"[setMotionAllowed=0]")
 
     def stop(self):
-        self.port.write("[stop=0]")
+        self.port.write(b"[stop=0]")
 
     def setTargetVelocities(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
         if a is not None:
-            self.port.write("[setCommandA=" + str(a) + "]")
-        self.port.write("[setTargetVelocities=0]")  
+            self.port.write(b"[setCommandA=" + str(a).encode('ascii') + b"]")
+        self.port.write(b"[setTargetVelocities=0]")  
 
     def setMaxVelocities(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
         if a is not None:
-            self.port.write("[setCommandA=" + str(a) + "]")
-        self.port.write("[setMaxVelocities=0]") 
+            self.port.write(b"[setCommandA=" + str(a).encode('ascii') + b"]")
+        self.port.write(b"[setMaxVelocities=0]") 
         
     def setMaxAccelerations(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
         if a is not None:
-            self.port.write("[setCommandA=" + str(a) + "]")
-        self.port.write("[setMaxAccelerations=0]")     
+            self.port.write(b"[setCommandA=" + str(a).encode('ascii') + b"]")
+        self.port.write(b"[setMaxAccelerations=0]")     
 
     def setMicrostepping(self, step):
-        self.port.write("[setMicrostepping=" + str(step & 0xFF) + "]")
+        self.port.write(b"[setMicrostepping=" + str(step & 0xFF) + b"]")
 
     def gotoXYA(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
         if a is not None:
-            self.port.write("[setCommandA=" + str(a) + "]")
-        self.port.write("[gotoXYA=0]")
+            self.port.write(b"[setCommandA=" + str(a).encode('ascii') + b"]")
+        self.port.write(b"[gotoXYA=0]")
 
     def gotoXY(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
-        self.port.write("[gotoXY=0]")  
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
+        self.port.write(b"[gotoXY=0]")  
 
     def moveFR(self, f=None, r=None):
         if f is not None:
-            self.debugWrite("[setCommandX=" + str(f) + "]")
+            self.debugWrite(b"[setCommandX=" + str(f).encode('ascii') + b"]")
         if r is not None:
-            self.debugWrite("[setCommandY=" + str(r) + "]")
-        self.debugWrite("[moveFR=0]")
+            self.debugWrite(b"[setCommandY=" + str(r).encode('ascii') + b"]")
+        self.debugWrite(b"[moveFR=0]")
 
     def rotateTo(self, theta):
-        self.port.write("[rotateTo=" + str(theta) + "]")
+        self.port.write(b"[rotateTo=" + str(theta).encode('ascii') + b"]")
 
     def rotate(self, theta):  
-        self.port.write("[rotate=" + str(theta) + "]") 
+        self.port.write(b"[rotate=" + str(theta).encode('ascii') + b"]")
 
     def resetPosition(self, x=None, y=None, a=None):
         if x is not None:
-            self.port.write("[setCommandX=" + str(x) + "]")
+            self.port.write(b"[setCommandX=" + str(x).encode('ascii') + b"]")
         if y is not None:
-            self.port.write("[setCommandY=" + str(y) + "]")
+            self.port.write(b"[setCommandY=" + str(y).encode('ascii') + b"]")
         if a is not None:
-            self.port.write("[setCommandA=" + str(a) + "]")
-        self.port.write("[resetPosition=0]")
+            self.port.write(b"[setCommandA=" + str(a).encode('ascii') + b"]")
+        self.port.write(b"[resetPosition=0]")
         print(self.port.readline())
