8693a2ecbae0540e9a5a5d8427d532f4981a3fa3
xander-will <xander@cavecreations.net>
2019-04-12 13:37:33 -0400

    fuck filtering

--- motor_driver.py
+++ motor_driver.py
@@ -40,17 +40,18 @@ class MotorDriverInterface:
                 return int(command.split("=")[1].split("]")[0])
 
     def write(self, s):
-        old_cmd_cnt = self.findMCC(self.getTelemetry())
-        while True:
-            self.port.write(s)
-            if self.debug == True:
-                print(s)
-            time.sleep(3)
-            new_cmd_cnt = self.findMCC(self.getTelemetry())
-            if old_cmd_cnt < new_cmd_cnt:
-                return   # function exit
-            if self.debug == True:
-                print("Message failed, sending again")
+        self.port.write(s)
+        # old_cmd_cnt = self.findMCC(self.getTelemetry())
+        # while True:
+        #     self.port.write(s)
+        #     if self.debug == True:
+        #         print(s)
+        #     time.sleep(3)
+        #     new_cmd_cnt = self.findMCC(self.getTelemetry())
+        #     if old_cmd_cnt < new_cmd_cnt:
+        #         return   # function exit
+        #     if self.debug == True:
+        #         print("Message failed, sending again")
 
     def setMotionAllowed(self, value):
         if value is True:
